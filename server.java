21fdb0e435ec18d5d7f3583cf7348ed9e9738b4f
ashu01 <as01hu@gmail.com>
2016-09-01 23:12:53 +0530

    1

--- server.java
+++ server.java
@@ -1,3 +1,51 @@
+// import java.io.*;
+// import java.net.*;
+// import java.util.*;
+ 
+// public class server {
+ 
+ 
+//     public static void main(String[] args) throws IOException {
+//         String port;
+//         BufferedReader stdIn =
+//                 new BufferedReader(
+//                     new InputStreamReader(System.in));
+//         System.out.println("Enter the port no : ");
+ 
+//         port=stdIn.readLine();
+ 
+//         int portNumber = Integer.parseInt(port);
+ 
+//         try {
+//             ServerSocket serverSocket =
+//                 new ServerSocket(portNumber);
+//             Socket clientSocket = serverSocket.accept();     
+//             PrintWriter out =
+//                 new PrintWriter(clientSocket.getOutputStream(), true);                   
+//             BufferedReader in = new BufferedReader(
+//                 new InputStreamReader(clientSocket.getInputStream()));
+ 
+//             String inputLine;
+//             System.out.println("Server Started");
+//             while (true) {
+//                 inputLine = in.readLine();
+//                 if(inputLine.equalsIgnoreCase("Exit"))
+//                 {
+//                      System.out.println("Exiting");
+//                      out.println("Server Exiting");
+//                      break;
+//                 }
+//                 out.println(System.currentTimeMillis()+5000);
+//             }
+//         } catch (IOException e) {
+//             System.out.println("Exception caught when trying to listen on port "
+//                 + portNumber + " or listening for a connection");
+//             System.out.println(e.getMessage());
+//         }
+//     }
+ 
+// }
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -9,7 +57,7 @@ public class server {
         String port;
         BufferedReader stdIn =
                 new BufferedReader(
-                    new InputStreamReader(System.in));
+                    new InputStreamReader(System.in));        hostName=
         System.out.println("Enter the port no : ");
  
         port=stdIn.readLine();
